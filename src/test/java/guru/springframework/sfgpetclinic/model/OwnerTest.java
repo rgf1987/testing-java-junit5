8457045661834e2acd81fc18bbad962b2cb64977
rgf1987 <rubenglez1987@gmail.com>
2023-06-25 23:49:29 +0200

    Enum & CSV Test
    Enum & CSV Test

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,8 +5,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import guru.springframework.sfgpetclinic.ModelTests;
 
@@ -29,6 +33,31 @@ class OwnerTest implements ModelTests{
 		);
 		
 		assertThat(owner.getCity(), is("Key West"));
+	}
+	
+	@DisplayName("Value Source Test")
+	@ParameterizedTest(name = "{displayName} - {index} {arguments}")
+	@ValueSource(strings = {"Spring", "Framework" ,"Guru"})
+	void parameterizedTest(String val) {
+		System.out.println(val);
+	}
+	
+	@DisplayName("Enum Source Test")
+	@ParameterizedTest(name = "{displayName} - {index} {arguments}")
+	@EnumSource(OwnerType.class)
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
+	}
+	
+	@DisplayName("CSV Input Test")
+	@ParameterizedTest(name = "{displayName} - {index} {arguments}")
+	@CsvSource({
+		"FL, 1, 1",
+		"OH, 2, 2",
+		"MI, 3, 3"
+	})
+	void csvInputTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = " + val1 + ":" + val2);
 	}
 
 }

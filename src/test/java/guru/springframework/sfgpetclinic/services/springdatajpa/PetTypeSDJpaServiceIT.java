ce0143396c1dbd48a36429acd2d7374df3fc74fe
rgf1987 <rubenglez1987@gmail.com>
2023-06-26 23:50:41 +0200

    IntegrationTest
    New integrationTest

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetTypeSDJpaServiceIT {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindAll() {		
+	}
+
+	@Test
+	void testFindById() {		
+	}
+
+	@Test
+	void testSave() {
+	}
+
+	@Test
+	void testDelete() {
+	}
+
+	@Test
+	void testDeleteById() {
+	}
+
+}

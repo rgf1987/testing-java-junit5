3ddb1818ff5c36c35b1e18315c43053220ce4723
rgf1987 <rubenglez1987@gmail.com>
2023-06-29 21:56:32 +0200

    Mockito test
    Mockito test

---
+++ src/test/java/guru/springframework/InlineMockTest.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class InlineMockTest {
+
+	@Test
+	void testInlineMock(){
+		Map mapMock = mock(Map.class);
+		
+		assertEquals(mapMock.size(), 0);
+	}
+}
